f2a76b419724b46a44673e3bcb07cc6770a5dc1f
U-REDMOND\v-oniobi <v-oniobi@v-oniobi-1217.redmond.corp.microsoft.com>
2015-01-02 21:52:50 -0800

    RTT working for multiple pings.  Next, will extend to 100 and begin recording statistics to file

--- InetSpeedTesting/MainPage.xaml.cpp
+++ InetSpeedTesting/MainPage.xaml.cpp
@@ -20,15 +20,18 @@ void InetSpeedTesting::MainPage::Button_Click(Platform::Object^ sender, Windows:
 	auto flow = [this]
 	{
 		StreamSocket^ socket = ref new StreamSocket();
-		Array<String^>^ hostNames = ref new Array<String^>{ "google.com","bing.com","facebook.com","yahoo.com" };
+		Array<String^>^ hostNames = ref new Array<String^>{ "google.com","abc.com","bing.com","msn.com"};
 		unsigned int hostNamesSize = 4;
 		Vector<Datum^>^ resultsVector = ref new Vector<Datum^>();
 		_testList->ItemsSource = resultsVector;
-		auto procession = [socket, resultsVector, hostNames](unsigned int hosts) mutable
+		auto procession = [socket, resultsVector, hostNames]() mutable
 		{
-			return create_task(socket->ConnectAsync(ref new HostName(hostNames[hosts]), ref new String(L"80")))
-				.then([socket, resultsVector, hosts]() mutable
+			return create_task(socket->ConnectAsync(ref new HostName(hostNames[0]), ref new String(L"80")))
+				.then([socket, resultsVector, hostNames]() mutable
 			{
+				Array<StreamSocket^>^ streamSocket = ref new Array<StreamSocket^>(3);
+				for (auto i = 0; i < 3; i++) streamSocket[i] = ref new StreamSocket();
+				for (auto i = 0; i < 3; i++) streamSocket[i]->ConnectAsync(ref new HostName(hostNames[i + 1]), ref new String(L"80"));
 				float32 speed = socket->Information->RoundTripTimeStatistics.Min / 1000000.0F;
 				String^ possible("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
 				String^ aggregate = "";
@@ -37,17 +40,20 @@ void InetSpeedTesting::MainPage::Button_Click(Platform::Object^ sender, Windows:
 				DataWriter^ dataWriter = ref new DataWriter(socket->OutputStream);
 				dataWriter->WriteString(aggregate);
 				auto beginTime = clock();
-				create_task(dataWriter->StoreAsync()).then([beginTime, speed, socket, resultsVector, payloadSize, hosts](unsigned int previousTask) mutable
+				create_task(dataWriter->StoreAsync()).then([beginTime, speed, socket, streamSocket, resultsVector, payloadSize](unsigned int previousTask) mutable
 				{
 					auto endTime = clock();
+					Array<DataWriter^>^ writer = ref new Array<DataWriter^>(3);
+					for (auto i = 0; i < 3; i++) writer[i] = ref new DataWriter(streamSocket[i]->OutputStream);
+					for (auto i = 0; i < 3; i++) writer[i]->StoreAsync();
 					auto uploadBandwidth = payloadSize / (float32)(endTime - beginTime) / 10 * CLOCKS_PER_SEC / 1024;
-					if (socket->Information->BandwidthStatistics.OutboundBandwidthPeaked == true) resultsVector->Append(ref new Datum(hosts, speed, socket->Information->BandwidthStatistics.OutboundBitsPerSecond / 1024.0F));
-					else resultsVector->Append(ref new Datum(hosts, speed, uploadBandwidth));
+					if (socket->Information->BandwidthStatistics.OutboundBandwidthPeaked == true) resultsVector->Append(ref new Datum(0, speed, socket->Information->BandwidthStatistics.OutboundBitsPerSecond / 1024.0F));
+					else resultsVector->Append(ref new Datum(0, speed, uploadBandwidth));
+					for (auto i = 0; i < 3; i++) resultsVector->Append(ref new Datum(i+1, streamSocket[i]->Information->RoundTripTimeStatistics.Min / 1000000.0F,streamSocket[i]->Information->BandwidthStatistics.OutboundBitsPerSecond / 1024.0F));
 				});
 			});
 		};
-		//procession(0);
-		procession(2);
+		procession();
 	};
 
 	// Main execution flow
